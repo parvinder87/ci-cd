ed78bd1d313664e17ba25513a6f6ded5945361af
parvinder87 <mca.parvinder@gmail.com>
2026-07-09 18:42:25 +0530

    Initial Commit

---
+++ src/main/java/com/BakeryApp/BakeryAppApplication.java
@@ -0,0 +1,13 @@
+package com.BakeryApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakeryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BakeryApp/BakeryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BakeryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
